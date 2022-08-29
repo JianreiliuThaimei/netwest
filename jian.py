15553e38c9960ffef5b34fad9693a33aae7c2fd9
unknown <jianreiliu21@navgurukul.org>
2022-08-29 22:50:29 +0530

    first commit

---
+++ jian.py
@@ -0,0 +1,32 @@
+
+
+string = 'GeeksforGeeks is a computer Science portal for Geeks'
+newstring = ''
+count1 = 0
+count2 = 0
+count3 = 0
+ 
+for a in string:
+ 
+    
+    if (a.isupper()) == True:
+        count1 += 1
+        newstring += (a.lower())
+ 
+    
+    elif (a.islower()) == True:
+        count2 += 1
+        newstring += (a.upper())
+ 
+ 
+    elif (a.isspace()) == True:
+        count3 += 1
+        newstring += a
+ 
+print("In original String : ")
+print("Uppercase -", count1)
+print("Lowercase -", count2)
+print("Spaces -", count3)
+ 
+print("After changing cases:")
+print(newstring)
